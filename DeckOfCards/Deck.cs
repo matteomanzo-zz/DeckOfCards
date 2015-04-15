946a8fd6b1d3bb84ec3cdb5a35eb279bcaf19667
Matteo Manzo <imatteo.manzo@gmail.com>
2015-04-15 18:19:19 +0100

    all tests pass

---
+++ DeckOfCards/Deck.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeckOfCards
+{
+	public class Deck
+	{
+		List<string> CardsDeck = new List<string>();
+
+		public Deck ()
+		{
+			CreateDeck ();
+		}
+
+		private List<string> CreateDeck()
+		{
+			string[] suits = { "Hearts", "Diamonds", "Spades", "Clubs" };
+			for (var x = 0; x <= 12; x++) {
+				foreach (string elem in suits) {
+					if (x == 0) {
+						CardsDeck.Add ("Ace" + elem);
+					} else if (x == 10) {
+						CardsDeck.Add ("Jack" + elem);
+					} else if (x == 11) {
+						CardsDeck.Add ("Queen" + elem);
+					} else if (x == 12) {
+						CardsDeck.Add ("King" + elem);
+					} else {
+						CardsDeck.Add (x.ToString () + elem);
+					}
+				}
+			}
+			return CardsDeck;
+		}
+
+		public int Count
+		{
+			get {
+				return CardsDeck.Count;
+			}
+		}
+
+		public string DealCard()
+		{
+			string deal = CardsDeck [0];
+			CardsDeck.RemoveAt (0);
+			return deal;
+		}
+	}
+}
+
---
+++ DeckOfCards/Test.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DeckOfCards
+{
+	[TestFixture ()]
+	public class DeckTest
+	{
+		Deck deck;
+
+		[SetUp()]
+		public void Init()
+		{
+			deck = new Deck ();
+		}
+
+		private bool DeckContainsCard(string card)
+		{
+			for (var x = 0; x <= deck.Count; x++) {
+				if (card == deck.DealCard()) {
+					return true;
+				}
+			};
+			return false;
+		}
+
+		[Test ()]
+		public void DeckShouldContainTheAceHeartsCard ()
+		{
+			Assert.IsTrue (DeckContainsCard ("AceHearts"));
+		}
+
+		[Test ()]
+		public void DeckShouldContain52Cards ()
+		{
+			Assert.AreEqual (52, deck.Count);
+		}
+
+		[Test ()]
+		public void DeckShouldContainFourSuits ()
+		{
+			var DeckContainsFourSuits = false;
+			DeckContainsFourSuits = DeckContainsCard ("AceHearts") && DeckContainsCard ("AceDiamonds") && DeckContainsCard ("AceSpades") && DeckContainsCard ("AceClubs");
+			Assert.IsTrue (DeckContainsFourSuits);
+		}
+
+		[Test ()]
+		public void DeckShouldDealTheTopCard ()
+		{
+			Assert.AreEqual (deck.DealCard(), "AceHearts");
+		}
+
+		[Test ()]
+		public void DeckShouldRemoveTheCardWhenDeal ()
+		{
+			deck.DealCard ();
+			Assert.AreEqual (51, deck.Count);
+		}
+
+		[Test ()]
+		public void DeckShouldNotHaveDuplicates ()
+		{
+			deck.DealCard ();
+			Assert.IsFalse (DeckContainsCard ("AceHearts"));
+		}
+			
+	}
+}
+
